95295b19a963ce5efd792fe07c1c07b67b2bef8e
Olaf Czarnecki <olaf.czarnecki@outlook.com>
2020-06-21 13:05:00 +0200

    timestamped logging

--- log_level.py
+++ log_level.py
@@ -2,35 +2,41 @@
 # Continuously log the crowd-level of boulderwelt-muenchen-ost.de
 
 import datetime
+import logging
 import pathlib
 import time
 
 from get_level import getCrowdLevel
 
 INTERVAL = 15 * 60  # interval between log entries [s]
-LOG_DIR  = 'log/'
 
-class Logger:
-    def __init__(self, logDir):
-        self.logDir = logDir if logDir[-1] == '/' else logDir + '/'
-        pathlib.Path(logDir).mkdir(parents=True, exist_ok=True)
+class Storage:
+    def __init__(self):
+        pathlib.Path('data/').mkdir(parents=True, exist_ok=True)
         
-    def logFileForNow(self):
-        return '%s%s.log' % (self.logDir, datetime.date.today())
+    def currentDataFileName(self):
+        return 'data/%s.csv' % (datetime.date.today())
         
-    def log(self, s):
-        with open(self.logFileForNow(), 'a') as logFile:
+    def record(self, s):
+        with open(self.currentDataFileName(), 'a') as logFile:
             logFile.write(s + '\n')
 
-def logCurrentLevel(logger):
+def recordCurrentLevel(storage):
     lvl = getCrowdLevel()
-    logger.log('%s,%s' % (datetime.datetime.now(), lvl))
+    storage.record('%s,%s' % (datetime.datetime.now(), lvl))
     
-def runLogLoop(logger):
+def runLogLoop(storage):
     while True:
-        logCurrentLevel(logger)
+        try:
+            recordCurrentLevel(storage)
+        except Exception as e:
+            logging.exception(e)
         time.sleep(INTERVAL)
         
     
 if __name__ == '__main__':
-    runLogLoop(Logger(LOG_DIR))
+    appStart = datetime.datetime.now().strftime('%Y-%m-%dT%H.%M.%S')
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s %(message)s",
+        filename="log/log_level-%s.log" % (appStart))
+    runLogLoop(Storage())
