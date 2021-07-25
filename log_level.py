b8918fc96c2bb4ac473f4a8a9f5da7b748203fdc
Olaf Czarnecki <olaf.czarnecki@outlook.com>
2021-07-25 13:48:13 +0100

    Added support for Vauxwall West

--- log_level.py
+++ log_level.py
@@ -6,41 +6,45 @@ import logging
 import pathlib
 import time
 
-from get_level import getCrowdLevel
+import scrapers
 from status_reporter import StatusReporter
 
 INTERVAL = 15 * 60  # interval between log entries [s]
 
+
 class Storage:
     def __init__(self):
         pathlib.Path('data/').mkdir(parents=True, exist_ok=True)
-        
+
     def currentDataFileName(self):
         return 'data/%s.csv' % (datetime.date.today())
-        
+
     def record(self, s):
+        print(s)
         with open(self.currentDataFileName(), 'a') as logFile:
             logFile.write(s + '\n')
 
-def recordCurrentLevel(storage):
-    lvl = getCrowdLevel()
-    storage.record('%s,%s' % (datetime.datetime.now(), lvl))
-    
-def runLogLoop(storage):
+
+def recordCurrentLevel(storage, scraper):
+    lvl = scraper.getCrowdLevel()
+    storage.record('%s,%s,%s' % (scraper.tag, datetime.datetime.now(), lvl))
+
+
+def runLogLoop(storage, scraper):
     while True:
         try:
-            recordCurrentLevel(storage)
+            recordCurrentLevel(storage, scraper)
         except Exception as e:
             logging.exception(e)
         time.sleep(INTERVAL)
-        
-    
+
+
 if __name__ == '__main__':
-    
     appStart = datetime.datetime.now().strftime('%Y-%m-%dT%H.%M.%S')
     logging.basicConfig(
         format="%(asctime)s %(levelname)s %(message)s",
         filename="log/log_level-%s.log" % (appStart))
-    
+
     reporter = StatusReporter("http://localhost:2048", "boulderlogger")
-    runLogLoop(Storage())
+    scraper = scrapers.ScraperVauxwall()
+    runLogLoop(Storage(), scraper)
